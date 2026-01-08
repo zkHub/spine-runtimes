38b32bde58520784fa2a02ef57223e84822dc215
KK <192938268@qq.com>
2026-01-08 17:50:15 +0800

    Add boundsProvider parameter to renderToImage function

--- spine-ios/Sources/Spine/Extensions/SkeletonDrawableWrapper+CGImage.swift
+++ spine-ios/Sources/Spine/Extensions/SkeletonDrawableWrapper+CGImage.swift
@@ -39,9 +39,10 @@ public extension SkeletonDrawableWrapper {
     ///     - size: The size of the `CGImage` that should be rendered.
     ///     - backgroundColor: the background color of the image
     ///     - scaleFactor: The scale factor. Set this to `UIScreen.main.scale` if you want to show the image in a view
-    func renderToImage(size: CGSize, backgroundColor: UIColor, scaleFactor: CGFloat = 1) throws -> CGImage? {
+    func renderToImage(size: CGSize, boundsProvider: BoundsProvider = SetupPoseBounds(), backgroundColor: UIColor, scaleFactor: CGFloat = 1) throws -> CGImage? {
         let spineView = SpineUIView(
             controller: SpineController(disposeDrawableOnDeInit: false), // Doesn't own the drawable
+            boundsProvider: boundsProvider,
             backgroundColor: backgroundColor
         )
         spineView.frame = CGRect(origin: .zero, size: size)
